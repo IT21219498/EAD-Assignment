ee6c77531cb3b44ac648771e751236fbdec06f48
IT21234484 <IT21234484@my.sliit.lk>
2024-09-30 22:39:54 +0530

    feature login and registration UI

--- EAD-Web/ead-web.client/src/AppRoutes.jsx
+++ EAD-Web/ead-web.client/src/AppRoutes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Inventory from "./pages/Inventory";
+import LoginForm from "./pages/LoginForm";
+import RegisterForm from "./pages/RegisterForm";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +11,9 @@ const AppRoutes = () => {
       <Route path='/' element={<Dashboard />} />
       <Route path='/Products' element={<Products />} />
       <Route path='/Inventory' element={<Inventory />} />
+      <Route path='/Login' element={<LoginForm />} />
+      <Route path='/Register' element={<RegisterForm />} />
+
     </Routes>
   );
 };
