c698f2f17e2ab95ddd769a562dcd147524d6bf64
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-10-03 01:20:40 +0530

    Add new Vendor API created

--- EAD-Web/EAD-Web.Server/Controllers/VendorController.cs
+++ EAD-Web/EAD-Web.Server/Controllers/VendorController.cs
@@ -8,10 +8,39 @@ namespace EAD_Web.Server.Controllers
     public class VendorController : ControllerBase
     {
         private readonly MongoDBContext _mongoContext;
+        private readonly ILogger<ProductsController> _logger;
 
-        public VendorController(MongoDBContext mongoContext)
+        public VendorController(MongoDBContext mongoContext, ILogger<ProductsController> logger)
         {
             _mongoContext = mongoContext;
+            _logger = logger;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult> CreateVendor([FromBody] Vendor vendor)
+        {
+            try
+            {
+                if (vendor == null)
+                {
+                    return BadRequest("Vendor object is null");
+                }
+
+                if (string.IsNullOrEmpty(vendor.vendorName) || string.IsNullOrEmpty(vendor.contactName) ||
+                    string.IsNullOrEmpty(vendor.contactNo) || string.IsNullOrEmpty(vendor.email) ||
+                    string.IsNullOrEmpty(vendor.category))
+                {
+                    return BadRequest("All required fields must be provided.");
+                }
+
+                await _mongoContext.Vendors.InsertOneAsync(vendor);
+                return Ok("Vendor created successfully.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in creating product");
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
