5a17376b05971b4547dce4f3a155e27b7c7e7bf7
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-10-05 21:08:28 +0530

    Create Add new vendor function in the Vendor.jsx

--- EAD-Web/ead-web.client/src/apis/vendor.js
+++ EAD-Web/ead-web.client/src/apis/vendor.js
@@ -23,3 +23,29 @@ export const fetchVendors = async () => {
         throw error;
     }
 }
+
+export const saveVendor = async (vendor) => {
+
+    try {
+        const response = await fetch("api/Vendor/create", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(vendor),
+        });
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("application/json")) {
+            throw new TypeError("Oops, we haven't got JSON!");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
+}
